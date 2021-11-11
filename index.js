5bf6a671ded01fd1be83e184e9a89dc61455246e
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-11-11 15:58:19 +0100

    Clear user input after click

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ const addNamesToList = function (event) {
     let newNameToAdd = document.createElement("div")
     listDiv.appendChild(newNameToAdd)
     listDiv.innerHTML = listDiv.innerHTML + nameToList
-    
+    nameToGet.innerHTML=""  
 }
 
 let addNameClick = document.getElementById("btn-add")
 addNameClick.addEventListener("click", addNamesToList)
+
+const clearName = function (event) {
+    let nameToGet = document.getElementById("insert-name")
+    nameToGet.value = ""
+}
+
+let clearInput = document.getElementById("btn-add")
+clearInput.addEventListener("click", clearName)
 
 window.onload = function () {
 
